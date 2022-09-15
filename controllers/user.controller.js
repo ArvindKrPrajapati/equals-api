956bdc838f85d73d5ed8ec2bae342c75be4baaa2
ArvindKrPrajapati <arvindsoft37@gmail.com>
2022-09-15 18:19:56 +0530

    updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,6 @@
 const user = require("../modals/user.modal")
-// const post = require("../modals/post.modal")
-// const follow = require("../modals/follow.modal")
+const post = require("../modals/post.modal")
+const follow = require("../modals/follow.modal")
 const jwt = require('jsonwebtoken');
 
 const mongoose = require('mongoose');
@@ -34,34 +34,38 @@ const getLoggedInUserInfo = async (req, res) => {
     }
 }
 const getUserById = async (req, res) => {
-    // try {
-    //     let _id = req.query.id
-    //     if (!_id) {
-    //         return res.status(404).json({ success: false, message: "id is not provided" })
-    //     }
-    //     if (mongoose.Types.ObjectId.isValid(_id)) {
-    //         _id = mongoose.Types.ObjectId(_id)
-    //         const userInfo = await user.findOne({ _id }).select('_id name dp')
-    //         if (!userInfo) {
-    //             return res.status(404).json({ success: false, message: 'user not found' })
-    //         }
-    //         const shorts =await post.find({postedby:_id}).count()
-    //         const ifollow = await follow.findOne({to:_id,by:req.userid}).count()
-    //         const followers=await follow.find({to:_id}).count()
-    //         const followings=await follow.find({by:_id}).count()
+    try {
+        let _id = req.query.id
+        if (!_id) {
+            return res.status(404).json({ success: false, message: "id is not provided" })
+        }
+        if (mongoose.Types.ObjectId.isValid(_id)) {
+            _id = mongoose.Types.ObjectId(_id)
+            const userInfo = await user.findOne({ _id }).select('_id name dp about dob gender')
+            if (!userInfo) {
+                return res.status(404).json({ success: false, message: 'user not found' })
+            }
+            const posts =await post.find({postedby:_id}).count()
+            const ifollow = await follow.findOne({to:_id,by:req.userid}).count()
+            const followers=await follow.find({to:_id}).count()
+            const followings=await follow.find({by:_id}).count()
 
-    //         return res.status(200).json({ success: true, data:{
-    //             name:userInfo.name,_id:userInfo._id,desc:userInfo.desc,image:userInfo.image,isVarified:userInfo.isVarified,
-    //             shorts:shorts,ifollow:ifollow==0 ? false : true,
-    //             followers,followings
-    //         } })
-    //     } else {
-    //         return res.status(401).json({ success: false, message: "invalid id" })
-    //     }
-    // } catch (error) {
-    //     console.log(error);
-    //     return res.status(500).json({ success: false, message: "server error" })
-    // }
+            return res.status(200).json({ success: true, data:{
+                name:userInfo.name,
+                _id:userInfo._id,
+                about:userInfo.about,
+                posts:posts,
+                ifollow:ifollow==0 ? false : true,
+                followers,
+                followings
+            } })
+        } else {
+            return res.status(401).json({ success: false, message: "invalid id" })
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: "server error" })
+    }
 }
 
 const liveSearch = async (req, res) => {
